2a32de8ddf1320d6e54dc6b652fd29126209a9a5
fedeesti <fede.estigarribia@hotmail.com>
2022-11-27 19:41:54 -0300

    Add project skeleton
    All dependencies necessary to start the project are installed. Configure linter, typescrit and add tailwind. In addition, the data folder on which the project is going to work is added.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'emerald-light': '#C9E4CA',
+        'emerald-dark': '#87BBA2',
+        'teal-light': '#55828B',
+        'teal-dark': '#3B6064',
+        'cyan-light': '#364958',
+        'cyan-dark': '#233D4D',
+        'body-linear-first': '#09203f',
+        'body-linear-second': '#537895',
+      },
+    },
+  },
+  plugins: [],
+};
